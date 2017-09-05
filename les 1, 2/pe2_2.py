f1fe7a84a9eb52fec7a6cf655f25d68bd789fc26
Noah van Bochove <noahlvb@gmail.com>
2017-09-05 16:04:01 +0200

    Added some more comments

--- les 1, 2/pe2_2.py
+++ les 1, 2/pe2_2.py
@@ -1,11 +1,15 @@
+# Cijfers worden ingevoerd
 cijferICOR = 6.8
 cijferPROG = 7.2
 cijferCSN = 6.3
 
+# Cijfers worden opgeteld
 cijfersTotaal = cijferICOR + cijferPROG + cijferCSN
 
+#Verschillende data wordt berekend
 gemiddelde = (cijfersTotaal)/3
 beloning = cijfersTotaal * 30
 overzicht = 'Mijn cijfers (gemiddeld een ' + str(gemiddelde) + ') leveren een beloning van ' + str(beloning) + 'Euro op!'
 
+#overzicht wordt uitgeprint
 print(overzicht)
