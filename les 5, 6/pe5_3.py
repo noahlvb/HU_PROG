9acf3566692cdfed7ed0e0a2a53ddf6a3cc1291a
Noah van Bochove <noahlvb@gmail.com>
2017-09-11 19:21:47 +0200

    Added pe5_3

--- les 5, 6/pe5_3.py
+++ les 5, 6/pe5_3.py
@@ -8,12 +8,13 @@ kaartnummers = readFile('kaartnummers.txt')
 kaartnummers = kaartnummers.splitlines()
 
 kaartRegels = len(kaartnummers)
-hoogsteKaartnummer = list()
+hoogsteKaartnummer = list([0, 0])
 
-for regelnummer, nummer in kaartnummers:
-    print(regelnummer)
-    #nummerNaam = nummer.split(',')
-    #if nummerNaam[0] > hoogsteKaartnummer[0]:
-    #    hoogsteKaartnummer[0] = nummerNaam[0]
-    #    hoogsteKaartnummer[1] = regelnummer + 1
-#print(hoogsteKaartnummers)
+for regelnummer, nummer in enumerate(kaartnummers):
+    nummerNaam = nummer.split(',')
+    if int(nummerNaam[0]) > int(hoogsteKaartnummer[0]):
+        hoogsteKaartnummer[0] = nummerNaam[0]
+        hoogsteKaartnummer[1] = regelnummer + 1
+
+print('Deze file telt ' + str(kaartRegels) + ' regels')
+print('Het grootste kaartnummer is: ' + str(hoogsteKaartnummer[0]) + ' en dat staat op regel ' + str(hoogsteKaartnummer[1]))
